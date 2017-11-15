bd35803e59925729d16cb85c75804bb12a14dde2
Tienson Qin <tiensonqin@gmail.com>
2017-11-15 22:27:15 +0800

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shadow-cljs/lein-template "0.1.0"
+(defproject shadow-cljs/lein-template "0.1.1"
   :description "Shadow-cljs template."
   :url "https://github.com/tiensonqin/shadow-cljs-template"
   :license {:name "Eclipse Public License"
