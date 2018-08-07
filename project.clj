b68374d73de4df7cc15fa4c39f9ee41fd5b3e5ed
Thomas Heller <th.heller@gmail.com>
2018-08-07 08:37:02 +0200

    bump 0.1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject shadow-cljs/lein-template "0.1.2"
+(defproject shadow-cljs/lein-template "0.1.3"
   :description "Shadow-cljs template."
   :url "https://github.com/tiensonqin/shadow-cljs-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
   :eval-in-leiningen true)
